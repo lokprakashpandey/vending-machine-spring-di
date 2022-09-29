6967a17153508546afca4d0fe1c5fb228435d0b4
Lok Prakash Pandey <lokprakashpandey@gmail.com>
2022-09-29 10:52:40 -0400

    M3-VendingMachine documentation for interfaces done

--- VendingMachine/src/main/java/com/lokpandey/vendingmachine/dao/VendingMachineDao.java
+++ VendingMachine/src/main/java/com/lokpandey/vendingmachine/dao/VendingMachineDao.java
@@ -13,10 +13,25 @@ import java.util.Map;
 
 public interface VendingMachineDao {
     
+    /**
+     * Selects the inventory file into a HashMap.
+     *
+     * @param void 
+     * @return Map<Item,Integer> of the inventory
+     * @throws InventoryPersistenceException when file cannot be read
+    */
     Map<Item, Integer> selectAllInventory() throws InventoryPersistenceException;
     
     //List<Item> selectNonZeroInventory() throws InventoryPersistenceException;
     
+    
+    /**
+     * Updates the inventory by decrementing the quantity of Item by 1.
+     *
+     * @param Item item object whose associated quantity is to be decremented
+     * @return void
+     * @throws InventoryPersistenceException when inventory file cannot be saved
+    */
     void updateInventory(Item item) throws InventoryPersistenceException;
                 
 }
--- VendingMachine/src/main/java/com/lokpandey/vendingmachine/service/VendingMachineServiceLayer.java
+++ VendingMachine/src/main/java/com/lokpandey/vendingmachine/service/VendingMachineServiceLayer.java
@@ -17,14 +17,46 @@ import java.util.Map;
  */
 public interface VendingMachineServiceLayer {
     
+    /**
+     * Selects the complete inventory into a Map.
+     *
+     * @param void
+     * @return Map<Item, Integer> from the inventory
+     * @throws InventoryPersistenceException if the inventory cannot be read
+    */
     Map<Item, Integer> serveAllInventory() throws InventoryPersistenceException;
     
-    //List<Item> serveNonZeroInventory() throws InventoryPersistenceException;
-    
+    /**
+     * Checks to see the capability to buy.
+     *
+     * @param BigDecimal money is the money to buy the item
+     * @param BigDecimal cost is the cost to buy the item
+     * @return true if money is equal to or more than cost
+     * @throws InsufficientFundsException when money is less than cost
+    */
     boolean isCapableToBuy(BigDecimal money, BigDecimal cost) throws InsufficientFundsException;
+    
+    
+    /**
+     * Checks to see the if there is stock for the item in inventory or not.
+     *
+     * @param Item item is the item to be checked in inventory
+     * @param Map<Item, Integer> is the inventory map to be checked against
+     * @return false if there is at least one item for the item name in the inventory
+     * @throws NoItemInventoryException when the stock for the item is zero
+    */
     
     public boolean isNoItemInInventory(Item item, Map<Item, Integer> map) 
             throws NoItemInventoryException;
+    
+    /**
+     * Updates the inventory by decrementing the quantity of Item by 1.
+     *
+     * @param Item item object whose associated quantity is to be decremented
+     * @return true if inventory file is updated and audit file is written
+     * @throws InventoryPersistenceException when inventory file cannot be saved 
+     * or audit file is not written
+    */
     
     boolean updateInventory(Item item) throws InventoryPersistenceException;
     
